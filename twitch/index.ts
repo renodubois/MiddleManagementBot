fff938f67057a7e39bfb5130ffeb55ca81f4b07e
Reno DuBois <renodubois@users.noreply.github.com>
2018-04-05 20:31:26 -0500

    Add Axios interface for Legacy API

--- twitch/index.ts
+++ twitch/index.ts
@@ -7,3 +7,9 @@ export const twitchAPI = axios.create({
   timeout: 1000,
   headers: {'Authorization': `Bearer ${secrets.TWITCH_ACCESS}`}
 });
+
+export const legacyTwitchAPI = axios.create({
+  baseURL: "https://api.twitch.tv/kraken/",
+  timeout: 1000,
+  headers: {'Authorization': `Bearer ${secrets.MAIN_TWITCH_ACCESS}`}
+})
