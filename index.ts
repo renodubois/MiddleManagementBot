ab9f2c4e3584e24837e7c953f02ed7435303bff6
Reno DuBois <renodubois@users.noreply.github.com>
2018-04-05 20:31:40 -0500

    Add userstate interface

--- index.ts
+++ index.ts
@@ -1,13 +1,37 @@
 import { secrets } from './secrets';
-import * as tmi from 'tmi.js';
+import tmi from 'tmi.js';
 import * as mongoose from 'mongoose';
-import * as users from './db/users';
+import { addNewUser, isUserPresent } from './db/users';
+import { isUserFollowing } from './twitch/users';
+
+interface USERSTATE {
+  badges: {
+    broadcaster: string,
+    subscriber: string,
+    premium: string
+  },
+  color: string,
+  'display-name': string,
+  emotes: any,
+  id: string,
+  mod: boolean,
+  'room-id': string,
+  subscriber: boolean,
+  'tmi-sent-ts': string,
+  turbo: boolean,
+  'user-id': string,
+  'user-type': any,
+  'emotes-raw': any,
+  'badges-raw': string,
+  username: string,
+  'message-type': string
+};
 
 // Connect to DB
 mongoose.connect('mongodb://' + secrets.DB_USER + ':' + secrets.DB_PASS + '@ds227199.mlab.com:27199/twitch-bot')
 const db = mongoose.connection;
 // Wait until we connect to the DB before doing anything else
-db.once('open', () => {
+db.once('open', async () => {
   console.log('Connected to DB!');
   // Init our Twitch client
   let client = new tmi.client({
@@ -20,24 +44,30 @@ db.once('open', () => {
       password: secrets.TWITCH_OAUTH
     }
   })
-  
+
   client.connect().then((data: object) => {
-    console.log(data) 
+    console.log(data)
   }).catch((err: Error) => {
     console.log("Error: ", err)
   })
-  
-  client.on("chat", (channel: string, userstate: object, message: string, self: boolean) => {
+
+  client.on("chat", async (channel: string, userstate: USERSTATE, message: string, self: boolean) => {
     if (self) {
       return;
     }
 
-    if (message === '!garbage') {
-      users.isUserPresent('clearlynotarealuser').then((res) => {
-        console.log(res);
-      }).catch((err: Error) => {
-        console.log(err.message);
-      })
+    const followingSplit = message.split("!following ");
+    console.log("Following Split: ", followingSplit);
+    if (followingSplit.length == 2) {
+      try {
+        const response = await isUserFollowing(followingSplit[1], client);
+        client.say(channel, `Is following: ${response}`);
+        return;
+      } catch(err) {
+        console.log(err);
+        client.say(channel, "You errored, you big dummy")
+        return;
+      }
     }
     client.say(channel, 'Hello world!');
   })
@@ -49,9 +79,9 @@ db.once('open', () => {
     }
     // Check to see if we have that user in the database
     try {
-      let userExists = await users.isUserPresent(username);
+      let userExists = await isUserPresent(username);
       if (!userExists) {
-        await users.addNewUser(username);
+        await addNewUser(username, client);
         console.log('Created new user!');
       }
     }
